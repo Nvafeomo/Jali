f25d639bf976bd000020afadc50873a2560141b2
Nvafeomo Konneh <nvafeomo05@gmail.com>
2026-06-04 01:49:23 -0400

    initialized

---
+++ CLAUDE.md
@@ -0,0 +1,1 @@
+This folder is for my Jali personal project which is a family tree type application where users input pictures and voice memos
---
+++ src/main/java/com/jali/JaliApplication.java
@@ -0,0 +1,13 @@
+package com.jali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaliApplication.class, args);
+	}
+
+}
