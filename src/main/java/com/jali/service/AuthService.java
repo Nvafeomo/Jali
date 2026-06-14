fead60be14821c07521811c5c151680d3809a0aa
Nvafeomo <nvafeomo05@gmail.com>
2026-06-13 21:27:37 -0400

    Added missing import to auth service

--- src/main/java/com/jali/service/AuthService.java
+++ src/main/java/com/jali/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.jali.service;
 
+import java.time.Instant;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
